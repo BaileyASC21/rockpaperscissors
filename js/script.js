9322b5c108d9485a1945b69c1b2a83d7524814a6
Glitch (rock-paper-scissors23) <none>
2021-12-08 23:18:27 +0000

    🍃✍️ Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 
+
+
+$(".play").click(function(){
+  
+  let userChoice = $(".input").val();
+  $(".userChoice").text(userChoice);
+  
+  let computerChoice = Math.ceil(Math.random()*3);
+
+  $(".computerChoice").text(computerChoice);
+  
+   if (computerChoice === 1){
+  let computerChoice = "rock";
+    $(".computerChoice").text(computerChoice);
+ } else if(computerChoice === 2){
+   let computerChoice = "paper";
+   $(".computerChoice").text(computerChoice);
+ } else if(computerChoice === 3){
+    let computerChoice = "scissors";
+   $(".computerChoice").text(computerChoice);
+ } 
+
+});
+
+//
+
+
+
